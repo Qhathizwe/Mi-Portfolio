16fffddb7ff4be4492511899a2a22312f330ea19
qhathizwe <sphoswa2@gmail.com>
2026-09-08 15:36:28 +0200

    project initialization

---
+++ src/components/nav-bar/NavBar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Navbar.module.css';
+
+export const Navbar: React.FC = () => {
+  return (
+    <nav className={styles.navbar}>
+      <Link to="/" className={styles.logo}>SP.</Link>
+      <div className={styles.navLinks}>
+        <Link to="/" className={styles.link}>HOME</Link>
+        <Link to="/about" className={styles.link}>ABOUT</Link>
+        <Link to="/projects" className={styles.link}>PROJECTS</Link>
+        <Link to="/contact" className={styles.contactBtn}>CONTACT</Link>
+      </div>
+    </nav>
+  );
+};
