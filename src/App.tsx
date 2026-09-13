99036c0324cb7f63219bb5bc56e0e3325c46b7e4
scelophoswagit config --global user.email sphoswa2@gmail.comgit config --global user.name scelo <sphoswa2@gmail.com>
2026-09-13 22:30:05 +0200

    changing routes and page arrangements

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 // src/App.tsx
-import {  Routes, Route } from 'react-router-dom';
+
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
-// 1. Structural Global Layout Elements
 import { Navbar } from './components/nav-bar/NavBar';
 import { Footer } from './components/footer/Footer';
 
-// 2. Individual Page Views
 import { Home } from './pages/home/Home';
 import { About } from './pages/about/About';
 import { Projects } from './pages/projects/Projects';
@@ -14,23 +13,29 @@ import { Contact } from './pages/contact/Contact';
 
 function App() {
   return (
+    <div className="app-container">
 
-      <div className="app-container">
-       
-        <Navbar />
-        
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
-        
-       
-        <Footer />
-      </div>
+      <Navbar />
+
+      <main className="main-content">
+
+        <Routes>
+
+          <Route path="/" element={<Home />} />
+
+          <Route path="/about" element={<About />} />
+
+          <Route path="/projects" element={<Projects />} />
+
+          <Route path="/contact" element={<Contact />} />
+
+        </Routes>
+
+      </main>
+
+      <Footer />
+
+    </div>
   );
 }
 
